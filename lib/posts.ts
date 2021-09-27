f7fc78d7260c40fb1299ddd601be18445110402b
oceanlvr <657531018@qq.com>
2021-09-27 12:04:21 +0800

    feat(post): support nested blog router

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,14 +1,57 @@
 
 import fs from 'fs'
 import { join } from 'path'
+import getAllFilesRecursively from '../lib/files'
+import { formatSlug } from '../lib/mdx'
+import matter from 'gray-matter'
+import { serialize } from 'next-mdx-remote/serialize'
 
-const postsDirectory = join(process.cwd(), '_posts')
 
-export function getLocalPostSourceBySlug(slug: string): string | null {
-  const fullPath = join(postsDirectory, `${slug}.md`)
-  return fs.existsSync(fullPath) ? fs.readFileSync(fullPath, 'utf8') : null
+const root: string = process.cwd()
+const dataDirectory = join(root, 'data')
+
+export async function getSourceBySlug(type: string, slug: string) {
+  const mdxPath = join(dataDirectory, type, `${slug}.mdx`)
+  const mdPath = join(dataDirectory, type, `${slug}.md`)
+  const source = fs.existsSync(mdxPath)
+    ? fs.readFileSync(mdxPath, 'utf8')
+    : fs.readFileSync(mdPath, 'utf8')
+  const { data, content } = matter(source)
+
+  const mdxSource = await serialize(content, {
+    scope: data
+  })
+
+  return {
+    mdxSource,
+    frontMatter: data,
+  }
 }
 
-export function getLocalPostSources(): string[] {
-  return fs.existsSync(postsDirectory) ? fs.readdirSync(postsDirectory, 'utf8').map(slug => slug.replace(/\.md/, '')) : []
+export function getSources(type: string): string[] {
+  const prefixPaths = join(dataDirectory, type)
+  const files = getAllFilesRecursively(prefixPaths)
+  return files.map((file: string) => file.slice(prefixPaths.length + 1))
+}
+
+const dateSortDesc = (a: string, b: string) => (a > b ? -1 : 1)
+
+export async function getAllFilesFrontMatter(folder: string) {
+  const prefixPaths = join(dataDirectory, folder)
+
+  const files = getAllFilesRecursively(prefixPaths)
+
+  const allFrontMatter: { [key: string]: any, id: string }[] = []
+
+  files.forEach((file: string) => {
+    // Replace is needed to work on Windows
+    const fileName = file.slice(prefixPaths.length + 1).replace(/\\/g, '/')
+    const source = fs.readFileSync(file, 'utf8')
+    const { data } = matter(source)
+    if (data.draft !== true) {
+      allFrontMatter.push({ ...data, id: formatSlug(fileName) })
+    }
+  })
+
+  return allFrontMatter.sort((a, b) => dateSortDesc(a.date, b.date))
 }
---
+++ pages/posts/[...id].tsx
@@ -0,0 +1,80 @@
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { useRouter } from 'next/router'
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
+import matter from 'gray-matter'
+import { getSourceBySlug, getSources, getAllFilesFrontMatter } from '../../lib/posts'
+import Heading from '../../components/Heading'
+import { formatSlug } from '../../lib/mdx'
+
+type ContextParams = {
+  id: string[]
+}
+
+interface Post {
+  mdxSource: MDXRemoteSerializeResult
+  frontMatter: Record<string, unknown>
+}
+
+interface Props {
+  post: Post
+  prev: {
+    [key: string]: any;
+    id: string;
+  }
+  next: {
+    [key: string]: any;
+    id: string;
+  }
+}
+
+const components = { ...Heading }
+
+export const getStaticProps: GetStaticProps<MDXRemoteSerializeResult, ContextParams> = async ({ params }) => {
+  const allPosts = await getAllFilesFrontMatter('_post')
+  if (!params) {
+    return {
+      notFound: true
+    }
+  }
+  const postIndex = allPosts.findIndex((post) => formatSlug(post.id) === params.id.join('/'))
+
+  const prev = allPosts[postIndex + 1] || null
+  const next = allPosts[postIndex - 1] || null
+  const post = await getSourceBySlug('_post', params.id.join('/'))
+  return {
+    props: {
+      post,
+      prev,
+      next,
+      compiledSource: post.mdxSource.compiledSource
+    }
+  }
+}
+
+export const getStaticPaths: GetStaticPaths<ContextParams> = async () => {
+  const posts = getSources('_post')
+  const paths = posts.map(post => ({
+    params: { id: formatSlug(post).split('/') },
+  }))
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+const Posts = ({ post, prev, next }: Props) => {
+  const { frontMatter, mdxSource } = post
+  const router = useRouter()
+  const { id } = router.query
+  return (
+    <div className="wrapper">
+      <div>slug: {id}</div>
+      <div>frontMatter: {frontMatter.title}</div>
+      <div>mdxSource:</div>
+      <MDXRemote {...mdxSource} components={components} />
+    </div>
+  )
+}
+
+export default Posts
+
