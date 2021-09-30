e6c65fe5dbfc7e66df3b764f897840e5dd871ad3
oceanlvr <657531018@qq.com>
2021-09-30 09:21:42 +0800

    fix(build): tailwind css import issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
+  mode: 'jit',
   theme: {
     extend: {
       fontFamily: {
